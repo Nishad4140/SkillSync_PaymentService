7968e5087cef05afb33ce1a201cfa913bb3d9478
Nishad Shanid <nishadshanid40@gmail.com>
2024-05-03 16:44:31 +0530

    completed payment service

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,23 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Nishad4140/SkillSync_PaymentService/db"
+	"github.com/Nishad4140/SkillSync_PaymentService/initializer"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load("../.env"); err != nil {
+		log.Fatal("error loading env file")
+	}
+	addr := os.Getenv("DB_KEY")
+	DB, err := db.InitDB(addr)
+	if err != nil {
+		log.Fatal("error initialising database")
+	}
+	initializer.Initializer(DB).Start(":4009")
+
+}
